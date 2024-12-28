e57e66166964cf2e92511f20ebf6a66262783481
Allison <allisonpower7@gmail.com>
2024-12-28 16:01:50 -0500

    08

---
+++ 08_Qt_Smart_Pointers/build/Desktop_Qt_6_8_0-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_08_Qt_Smart_Pointers_FILE /home/allison/Documents/Qt6_Tutorials/08_Qt_Smart_Pointers/build/Desktop_Qt_6_8_0-Debug/08_Qt_Smart_Pointers)
+set(__QT_DEPLOY_TARGET_08_Qt_Smart_Pointers_TYPE EXECUTABLE)
